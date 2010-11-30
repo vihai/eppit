06ecd88f49b4213ee9f8ff48cc4f4207e652f0f7
Daniele Orlandi <daniele@orlandi.com>
2010-11-30 02:14:07 +0100

    Added response text in EPPit exceptions

--- lib/eppit/exceptions.rb
+++ lib/eppit/exceptions.rb
@@ -3,12 +3,14 @@ class Session
 
 class ErrorResponse < StandardError #:nodoc:
   attr_accessor :epp_message
+  attr_accessor :response
   attr_accessor :response_code
   attr_accessor :reason
   attr_accessor :reason_code
 
   # Generic EPP exception. Accepts a response code and a message
   def initialize(epp_msg)
+    @response = epp_msg.response.result.msg
     @response_code = epp_msg.response.result.code
     @reason = epp_msg.response.result.ext_value ? epp_msg.response.result.ext_value.reasons['en'] : nil
     @reason_code = epp_msg.response.result.ext_value ? epp_msg.response.result.ext_value.reason_code : nil
